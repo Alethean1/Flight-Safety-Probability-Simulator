33454ae2c0a3a1d55f675a3320b35873419839c9
Alethean1 <81870154+Alethean1@users.noreply.github.com>
2021-11-14 18:50:26 -0500

    Add files via upload

---
+++ flight_app_final.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+prompt = input ("Take a flight? or Automate flights?")
+
+
+def Probability():
+        auto_flights= random.randint(1,1000000)
+        print(auto_flights)
+        
+        if auto_flights == 500000:
+                print("Crash")
+        else:
+                print("Safe Flight")
+                
+
+if prompt == "Take a flight":
+        print("Flying")
+        time.sleep(1)
+        Probability()
+while prompt == "Automate flights":
+        Probability()
+
